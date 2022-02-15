95845a26c8313b25b6063823535a91f5a92ba632
DOH-FXX0303 <fan.xiong@doh.wa.gov>
2022-02-15 11:23:07 -0800

    add new R script

---
+++ Parallel test.R
@@ -0,0 +1,11 @@
+library(tidyverse) # ggplot2, purrr, dplyr, tidyr, readr, tibble
+library(vroom) # reading csv files fast with implicit parallelism
+library(parallel) # parallel processing & backend for doParallel
+library(doParallel) # parallel processing (foreach loops)
+library(furrr) # parallel implementation of purrr 
+library(future) # backend for furrr
+library(multidplyr) # parallel processing
+
+##I don't like seeing scientific notations in my results
+options(scipen = 999)
+
